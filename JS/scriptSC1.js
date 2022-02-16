c85d22e9ab924c9ab51b911ac9c9b7d469c01203
Victor Hugo Lessa <victor.lessa@estudante.ufla.br>
2022-02-16 17:41:42 -0300

    acabei a parte de clicar no quiz e abrir a tela 2

--- JS/scriptSC1.js
+++ JS/scriptSC1.js
@@ -1,29 +1,39 @@
 /*----- FUNÇÃO PARA OBTER QUIZZ DO SERVIDOR ----- */
 
 function obterQuizz(){
-const promiseQuizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
-promiseQuizz.then(mostrarQuizz)
-console.log(promiseQuizz)
+const promiseQuizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+promiseQuizz.then(console.log)
 }
+obterQuizz();
 
-function mostrarQuizz(resposta){
-    let i = 0;
-    let quizzes = resposta.data;
-    let listaQuizzesServidor = document.querySelector(".lista-quizz-servidor")
-    listaQuizzesServidor.innerHTML = "";
+function abrirTelaDois(){
+    let corpo = document.querySelector("main")
+    corpo.innerHTML = `<div class="tela-dois">
+    <div class="topo-quizz">
+        <p>Você entende bem sobre as praias do Brasil?</p>
+    </div>
 
-    for(i =0; i <= quizzes.length; i++){
-        const quizz = quizzes[i];
-        if(quizz.id === quizzes[i]){
-            listaQuizzesServidor.innerHTML += `
-            <div class="display-quizz">
-                <div class="imagem-quizz">
-                    <div class="titulo-quizz">Acerte os personagens corretos dos Simpsons e prove seu amor!
-                        </div>
-                </div>
-            </div>`
-
-        }
-    }
-
+    <div class="pergunta-quizz">
+        <div class="topo-pergunta-quizz">
+            <p>Em qual animal Olho-Tonto Moody transfigurou Malfoy?</p>
+        </div>
+        <div class="respostas-pergunta-quizz">
+            <div class="alternativa-respostas-pergunta-quizz">
+                <div class="img"></div>
+                <p>Gatíneo</p>
+            </div>
+            <div class="alternativa-respostas-pergunta-quizz">
+                <div class="img"></div>
+                <p>Gatíneo</p>
+            </div>
+            <div class="alternativa-respostas-pergunta-quizz">
+                <div class="img"></div>
+                <p>Gatíneo</p>
+            </div>
+            <div class="alternativa-respostas-pergunta-quizz">
+                <div class="img"></div>
+                <p>Gatíneo</p>
+            </div>
+        </div>
+    </div>`
 }
