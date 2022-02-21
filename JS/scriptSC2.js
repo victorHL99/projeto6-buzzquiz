851c9255fb9b0d5eaaa7572e8fbc175520805dc9
Victor Hugo Lessa <victor.lessa@estudante.ufla.br>
2022-02-20 23:08:30 -0300

    erro corrigido na scroll

--- JS/scriptSC2.js
+++ JS/scriptSC2.js
@@ -7,19 +7,13 @@ function escolherResposta(alternativaEscolhida){
     esbranquicarAlternativas();
 }
 
-// finção para esbranquiçar as opções que não foram escolhidas
+// função para esbranquiçar as opções que não foram escolhidas
 function esbranquicarAlternativas(){
     let adicionarOpacidade = document.querySelectorAll(".alternativa-respostas-pergunta-quizz")
     for(let i=0; i<adicionarOpacidade.length; i++){
     adicionarOpacidade[i].classList.add("diminui-opacidade")
     }
 }
-
-// se eu escolher uma resposta as outras tem q ganhar efeito esbranquiçado 
-
-
-// não deve ser possivel mudar a resposta
-
 // deve receber cor vermelha ou verde, de acordo com resposta certa ou errada
 
 // após 2 segundos deve passar para a proxima pergunta 
